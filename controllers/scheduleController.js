312cb15be2e91b73571e2963ae86130180d39abc
Samarth-Dengre <samarth12911@gmail.com>
2023-04-09 19:25:12 +0530

    Added functionality of fetching tasks wrt given hours per day and checked it via postman

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -2,4 +2,23 @@ const TwoHours = require("../models/twoHours");
 const FourHours = require("../models/fourHours");
 const SixHours = require("../models/sixHours");
 
-module.exports.scheduleHandler = async (req, res) => {};
+module.exports.scheduleHandler = async (req, res) => {
+  try {
+    const { hrs } = req.query;
+    if (hrs === "2") {
+      const data = await TwoHours.find();
+      return res.status(200).json(data);
+    } else if (hrs === "4") {
+      const data = await FourHours.find();
+      return res.status(200).json(data);
+    } else if (hrs === "6") {
+      const data = await SixHours.find();
+      return res.status(200).json(data);
+    } else {
+      return res.status(400).json({ message: "Bad request" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
